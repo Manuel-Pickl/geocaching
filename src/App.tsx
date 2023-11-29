574165e13319d02f7a05f1b38f432a174e28311a
Manuel Pickl <picklmanuel98@gmail.com>
2023-11-29 17:21:26 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import './App.css'
 import Map from './Map/Map'
 
 const mapCenter: [number, number] = [51.505, -0.09];
-const mapZoom: number = 13;
 
 function App() {
   return (
     <>
-      <Map center={mapCenter} zoom={mapZoom} />
+      <Map position={mapCenter} />
     </>
   )
 }
--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -3,65 +3,73 @@ import L, { LatLngExpression, Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Map.css'
 
-interface LeafletMapProps {
-  center: [number, number];
-  zoom: number;
+interface MapProps {
+  position: [number, number];
 }
 
 enum MapType {
-    Map,
-    Hybrid,
-    Satellite
-  }
+  Map,
+  Hybrid,
+  Satellite
+}
 
-const Leaflet: React.FC<LeafletMapProps> = ({ center, zoom }) => {
-  const mapRef = useRef<Map | null>(null);
+const Leaflet: React.FC<MapProps> = ({ position }) => {
+  const minZoom: number = 8;
+  const maxZoom: number = 22;
+  const startZoom: number = 13;
+  
+  const mapHtmlElement = useRef<Map | null>(null);
 
   useEffect(() => {
-    // Initialize the map
-    mapRef.current = L.map('map').setView(toLatLngExpression(center), zoom);
+    const map: L.Map = initializeMap();
 
-    getTileLayer(MapType.Hybrid).addTo(mapRef.current);
-
-    // Add a marker
-    L.marker(toLatLngExpression(center)).addTo(mapRef.current);
+    mapHtmlElement.current = map;
+    getTileLayer(MapType.Hybrid).addTo(mapHtmlElement.current);
 
     return () => {
       // Cleanup when the component unmounts
-      if (mapRef.current) {
-        mapRef.current.remove();
+      if (mapHtmlElement.current) {
+        mapHtmlElement.current.remove();
       }
     };
-  }, [center, zoom]);
+  }, [position, startZoom]);
 
   return <div id="map"></div>;
+
+  function initializeMap() {
+    return L.map('map').setView(toLatLngExpression(position), startZoom);
+  }
+
+  function toLatLngExpression(coordinates: [number, number]): LatLngExpression {
+    return L.latLng(coordinates);;
+  }
+
+  function getTileLayer(mapType: MapType): L.TileLayer {
+    // tileLayers found on https://stackoverflow.com/questions/33343881/leaflet-in-google-maps
+    switch(mapType) {
+      case MapType.Satellite:
+        // satellite
+        return L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+          minZoom: minZoom,
+          maxZoom: maxZoom,
+          subdomains:['mt0','mt1','mt2','mt3']
+        });
+      case MapType.Hybrid:
+        // hybrid
+        return L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+          maxZoom: maxZoom,
+          minZoom: minZoom,
+          subdomains:['mt0','mt1','mt2','mt3']
+        });
+      default:
+        // map
+        return L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+          maxZoom: maxZoom,
+          minZoom: minZoom,
+          subdomains:['mt0','mt1','mt2','mt3']
+        });
+    }
+  }
 };
 
 export default Leaflet;
-
-function toLatLngExpression(coordinates: [number, number]): LatLngExpression {
-    return L.latLng(coordinates);;
-}
-
-function getTileLayer(mapType: MapType): L.TileLayer {
-  switch(mapType) {
-    case MapType.Satellite:
-      // satellite
-      return L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-      });
-    case MapType.Hybrid:
-      // hybrid
-      return L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-      });
-    default:
-      // map
-      return L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-      });
-  }
-}
