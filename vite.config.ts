55b9298d67e446084cdf5ffd599489680abd5c68
Manuel Pickl <picklmanuel98@gmail.com>
2023-12-03 20:44:43 +0100

    pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import checker from 'vite-plugin-checker'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     checker({ typescript: true }),
-  ]
-})
+    VitePWA({
+      manifest: {
+        name: 'Geocaching',
+        short_name: 'Geocaching',
+        description: 'Erkunden Sie Ihre Umwelt!',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '/globe.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
