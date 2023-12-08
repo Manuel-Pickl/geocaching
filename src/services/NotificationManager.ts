bfb04c171a115afd5ce2fce68ba0b4dc5b9fce20
Manuel Pickl <picklmanuel98@gmail.com>
2023-12-08 22:24:24 +0100

    push test

--- src/services/NotificationManager.ts
+++ src/services/NotificationManager.ts
@@ -7,7 +7,13 @@ export const sendPushMessage = (
       registration.showNotification(title, { body: message });
     }
     else {
-      registration.showNotification(title);
+      registration.showNotification(
+        title, {
+          icon: "./markers/marker1.png",
+          image: "./markers/marker2.png",
+          badge: "./markers/marker3.png"
+        }
+      );
     }
   });
 }
