f9c44f613dd6f0252eeec2709dea80bb7f154937
Manuel Pickl <picklmanuel98@gmail.com>
2023-12-14 14:45:32 +0100

    credits style update

--- src/components/Tabs/Credits/Credits.tsx
+++ src/components/Tabs/Credits/Credits.tsx
@@ -18,33 +18,34 @@ function Credits({ isOpen }: CreditsProps)
     const libraries = [
         { name: "Leaflet", url: "https://leafletjs.com/" },
         { name: "qr-scanner", url: "https://www.npmjs.com/package/qr-scanner" },
-        { name: "fontawesome", url: "https://fontawesome.com/" },
         { name: "geolib", url: "https://deltares.github.io/GEOLib/latest/index.html" },
     ]
     
     const sources = [
-        { name: "React Learn", url: "https://react.dev/learn" },
-        { name: "Leaflet Docs", url: "https://leafletjs.com/reference.html" },
+        { name: "fontawesome", url: "https://fontawesome.com/" },
         { name: "SVG Repo", url: "https://www.svgrepo.com/" },
     ]
 
     const creditsData = [
-        { category: "Technologies", entries: technologies },
-        { category: "Libraries", entries: libraries },
-        { category: "Sources", entries: sources },
+        { category: "Technologien", entries: technologies, url: "technologies" },
+        { category: "Bibliotheken", entries: libraries, url: "libraries" },
+        { category: "Quellen", entries: sources, url: "sources" },
       ];
 
     return isOpen ? (
         <div className="tab credits">
             <h2>Credits</h2>
             {creditsData.map((credit, index) => (
-            <div key={index}>
+            <div key={index} className="category">
                 <h3>{credit.category}</h3>
                 <ul>
                     {credit.entries.map((entry, entryIndex) => (
                     <a key={entryIndex} href={entry.url} target="_blank">
                         <div className="icon">
-                            <img src={`credits/${credit.category.toLowerCase()}/${entry.name}.png`} alt={entry.name} />
+                            <img 
+                                src={`credits/${credit.url}/${entry.name}.png`}
+                                alt={entry.name}
+                            />
                         </div>
                         <span>{entry.name}</span>
                     </a>
